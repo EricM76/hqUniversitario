2770a2d4ff2416e018465abe4a1bd969a8145726
Jonatan Céspedes <develop.jec@gmail.com>
2022-09-04 19:00:08 -0300

    Agrega metodos en mainController

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,30 @@
+const db = require("../database/models")
+
 module.exports = {
     home : (req,res) => {
-        return res.render('finalUser/home')
+        /* Obtener Banners */
+        /* Obtener Universidades */
+        /* Obtener Últimas materias agregadas */
+        /* Obtener Materias populares */
+        /* const getBanners = db.Banner.findAll(); */
+        /*  const getMostPopularCourses = db.Course.findAll({
+            
+         }) */
+        const getUniversities = db.University.findAll();
+        const getLastestCourses = db.Course.findAll({
+            where: {
+                visible: true,
+            },
+            order: [["id", "DESC"]],
+            limit: 10,
+        });
+        Promise.all([getUniversities, getLastestCourses])
+        .then(([universities, lastestCourses]) => {
+            return res.render("finalUser/home", {
+                universities,
+                lastestCourses
+            })
+        })
     },
     admin : (req,res) => {
         return res.render('admin/index')
