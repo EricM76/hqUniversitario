5cf0d2c19e8cd92d8ac27917a6784d0e369659cc
Jonatan Céspedes <develop.jec@gmail.com>
2022-09-07 23:33:28 -0300

    Agrega materias relacionadas en presentacion de la materia

--- src/controllers/finalUser/courseController.js
+++ src/controllers/finalUser/courseController.js
@@ -1,6 +1,22 @@
+const db = require("../../database/models");
+
 module.exports = {
     presentation: (req, res) => {
-        res.render("finalUser/coursePresentation");
+        const courseId = req.params.id;
+        db.Course.findByPk(courseId)
+        .then((course) => {
+            db.Course.findAll({
+                where: {
+                    facultyId: course.facultyId,
+                }
+            })
+            .then((relatedCourses) => {
+                res.render("finalUser/coursePresentation", {
+                    course,
+                    relatedCourses
+                });
+            })
+        })
     },
     content: (req, res) => {
         res.render("finalUser/courseContent");
--- src/routes/finalUser/course.js
+++ src/routes/finalUser/course.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { content, presentation } = require("../../controllers/finalUser/courseController");
 
 router
-    .get("/presentacion/:id?", presentation)
+    .get("/presentacion/:id", presentation)
     .get("/contenido/:id?", content)
     
 module.exports = router;
