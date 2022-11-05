f7cd33627c42c7c5275cb7558da717a1004d5d67
Jonatan Céspedes <66534017+JonatanCespedes@users.noreply.github.com>
2022-11-05 17:39:31 -0300

    Agrega alert con los dias a vencer la menbresia

--- public/javascripts/Home/membershipActive.js
+++ public/javascripts/Home/membershipActive.js
@@ -1,36 +1,34 @@
 window.addEventListener("load", async () => {
-    const BASE_URL = window.location.origin;
-    const params = new URLSearchParams(document.location.search);
-    const userId = params.get("userId");
-if (userId) {
+  const BASE_URL = window.location.origin;
+  const params = new URLSearchParams(document.location.search);
+  const userId = params.get("userId");
+  if (userId) {
     try {
-        let response = await fetch(`${BASE_URL}/membresias/usuario/${userId}`)
-        if (response.ok) {
-            let json = await response.json();
-            if(json.quotasAvailable > 0) {
-                Swal.fire({
-                    title: `Tienes ${json.quotasAvailable} cupos disponibles`,
-                    text: "Elije tus materias antes de que expire tu membresía",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Vamos!'
-                  }).then((result) => {
-                    if (result.isConfirmed) {
-                      window.location.href = "/usuario/perfil#membership"
-                    }
-                  })
+      let response = await fetch(`${BASE_URL}/membresias/usuario/${userId}`);
+      if (response.ok) {
+        let json = await response.json();
+        if (json.quotasAvailable > 0) {
+          Swal.fire({
+            title: `Tienes ${json.quotasAvailable} cupos disponibles`,
+            text: "Elije tus materias antes de que expire tu membresía",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Vamos!",
+          }).then((result) => {
+            if (result.isConfirmed) {
+              window.location.href = "/usuario/perfil#membership";
             }
-            
+          });
         }
+      }
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
-}
+  }
 
-// Validar
-   
+  // Validar
 
-      // warning de vencimiento de membresia
-})
+  // warning de vencimiento de membresia
+});
