34ce5659e3dac1f5c268f5ff019efabfa0338d65
Eric <menaericdaniel@gmail.com>
2022-11-17 06:35:24 -0300

    fix req.headers.referer undefined

--- src/controllers/finalUser/userController.js
+++ src/controllers/finalUser/userController.js
@@ -23,7 +23,7 @@ module.exports = {
     const baseUrl = `${req.protocol}://${req.headers.host}`;
     const TIME_IN_MILISECONDS = 60000;
     console.log('>>>>>>>>>>>>>referer',req.headers.referer)
-      res.cookie("backurl", req.headers.referer ? req.headers.referer.split(baseUrl)[1] : "/", {
+      res.cookie("backurl", req.headers.referer !== 'undefined' ? req.headers.referer.split(baseUrl)[1] : "/", {
         expires: new Date(Date.now() + TIME_IN_MILISECONDS),
         httpOnly: true,
         secure: true,
