d73e3c98456fc840fbc3deba59087a0f61c2e7fb
Eric <menaericdaniel@gmail.com>
2022-12-16 04:50:41 -0300

    save sql updated memberships

---
+++ src/database/updateMembership.sql
@@ -0,0 +1,8 @@
+INSERT INTO hq_db.memberships (name,image,quota,price,description,createdAt,updatedAt,deletedAt,days,`order`) VALUES
+	 ('BASIC','defaultMembership.jpg',1,'900','Acceso a todo el contenido completo de 1 materia por 30 días','2022-12-10 14:16:48','2022-12-10 14:16:48',NULL,30,1),
+	 ('BASIC','defaultMembership.jpg',1,'1650','Acceso a todo el contenido completo de 1 materia por 90 días','2022-12-10 14:16:48','2022-12-10 14:16:48',NULL,90,2),
+	 ('PRO','defaultMembership.jpg',3,'1200','Acceso a todo el contenido completo de hasta 3 materias por 30 días','2022-12-10 14:16:48','2022-12-10 14:16:48',NULL,30,3),
+	 ('PRO','defaultMembership.jpg',3,'2250','Acceso a todo el contenido completo de hasta 3 materias por 90 días','2022-12-10 14:16:48','2022-12-10 14:16:48',NULL,90,4),
+	 ('PREMIUM','defaultMembership.jpg',5,'1500','Acceso a todo el contenido completo de hasta 5 materias por 30 días','2022-12-10 14:16:48','2022-12-10 14:16:48',NULL,30,5),
+	 ('PREMIUM','defaultMembership.jpg',5,'2850','Acceso a todo el contenido completo de hasta 5 materias por 90 días','2022-12-10 14:16:48','2022-12-10 14:16:48',NULL,90,6),
+	 ('FREE','defaultMembership.jpg',0,'0','Acceso solo al contenido de muestra','2022-12-10 14:16:48','2022-12-10 14:16:48',NULL,30,7);
