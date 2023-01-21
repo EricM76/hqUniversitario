689f6a9d5147f0dbec2bf05ed5f4eeeb01e2d0f5
Eric <menaericdaniel@gmail.com>
2023-01-21 16:09:22 -0300

    fix create new career

--- public/javascripts/career.js
+++ public/javascripts/career.js
@@ -1,12 +1,12 @@
-$('universityId').addEventListener('change',async ({target}) => {
+document.getElementById('universityId').addEventListener('change',async ({target}) => {
     try {
         let response = await fetch('/faculties/get-by-university?universityId=' + target.value);
         let result = await response.json()
-        /* console.log(result) */
+        console.log(result)
         if(result.ok){
-            $('facultyId').innerHTML = '<option value="" select hidden>Seleccione...</option>'
+            document.getElementById('facultyId').innerHTML = '<option value="" select hidden>Seleccione...</option>'
             result.data.forEach(faculty => {
-                $('facultyId').innerHTML +=`<option value="${faculty.id}">${faculty.name}</option>`
+                document.getElementById('facultyId').innerHTML +=`<option value="${faculty.id}">${faculty.name}</option>`
             })
         }else{
             throw new Error('upss, algo anduvo mal')
