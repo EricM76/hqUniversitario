bb256e19ae1d2d1a24809229381fd03e8d1b021e
Eric <menaericdaniel@gmail.com>
2023-03-05 22:20:43 -0300

    fix error.status videosController

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -438,7 +438,7 @@ module.exports = {
 
       } catch (error) {
         console.log(error)
-        return res.status(error.status).json({
+        return res.status(error.status || 500).json({
           ok: false,
           msg: 'ups... error'
         })
